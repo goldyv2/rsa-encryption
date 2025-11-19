d19f49dfeb21baef22a31c9726e94ce3753c67b1
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-11-19 23:11:06 +0000

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+<div align="center">
+
+  # rsa-encryption in 🐍
+
+</div>
+
+A partial, unoptimized and slow implementation of RSA encryption I wrote from scratch in Python just so I could understand the math behind it.
+
+> **This program is NOT to be considered a real or proper implementation of RSA encryption. This was created out of curiosity and to learn by implementing many fundamentals from scratch hence why I didn't just use the algorithms like (gcd) python provides.**
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This program is NOT to be considered a real or proper implementation of RSA encryption.
-# This was created out of curiosity and to learn by implementing many fundamentals from scratch.
+# This was created out of curiosity and to learn by implementing many fundamentals from scratch 
+# hence why I didn't just use the algorithms like (gcd) python provides.
 
 import secrets
 from typing import Optional
@@ -59,6 +60,7 @@ def find_decryption_exponent(encryption_exponent: int, rsa_module: int) -> Optio
     coprime_numbers_count = euler_totient(rsa_module)
 
     while decryption_exponent < coprime_numbers_count:
+        # (e x d) mod ϕ(n) = 1
         if (encryption_exponent * decryption_exponent) % coprime_numbers_count == 1:
             return decryption_exponent
 
@@ -67,19 +69,23 @@ def find_decryption_exponent(encryption_exponent: int, rsa_module: int) -> Optio
     # should never really happen if correct input is given.
     return None
 
-def generate_rsa_key_pairs(p: int, q: int) -> tuple[Key, Key]:
-    """
-    Generates and returns public key and private key from p and q prime numbers.
+if __name__ == "__main__":
+    # random prime numbers
+    p = 83
+    q = 59
 
-    The first key is the public key and second key is the private key.
-    """
+    # Generate a public key and private key from p and q prime numbers.
 
     # we get "n" by simply multiplying p and q.
     rsa_module = p * q # aka "n"
 
+    print(f"rsa module (n) --> {rsa_module}")
+
     # Now we need to calculate how many of the numbers less than or 
     # equal to "n" are coprime (two numbers have no common divisor except 1) 
     # to "n" so we can get a value for "e" and "d".
+
+    # encryption_exponent = 2519
     encryption_exponent = random_encryption_exponent(rsa_module) # aka "e"
     decryption_exponent = find_decryption_exponent(encryption_exponent, rsa_module) # aka "d"
 
@@ -93,14 +99,27 @@ def generate_rsa_key_pairs(p: int, q: int) -> tuple[Key, Key]:
         exponent = decryption_exponent
     )
 
-    return (public_key, private_key)
-
-if __name__ == "__main__":
-    # random prime numbers
-    p = 19
-    q = 71
-
-    public_key, private_key = generate_rsa_key_pairs(p, q)
-
     print("public ->", public_key)
     print("private ->", private_key)
+
+    # now time to actually encrypt a message and decrypt it with the pairs
+    message = 369
+
+    print(f"message (m) --> {message}")
+
+    if message > rsa_module or message < 0:
+        print("Message must be greater or equal to 0 and must be bigger than rsa module!")
+        exit(1)
+
+    # we now encrypt the message using the public key encryption exponent.
+    cipher = message ** public_key.exponent % public_key.rsa_module
+
+    print(f"cipher (c) --> {cipher}")
+
+    # now let's decrypt the message using the inverse formula
+    deciphered_message = cipher ** private_key.exponent % private_key.rsa_module
+
+    print(f"decrypted message (m) --> {deciphered_message}")
+
+    # same exact message
+    assert message == deciphered_message
